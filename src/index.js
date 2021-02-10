aa540efb44192d575d9b5164475a9083b524cd73
Jaeri Park <59538964+motonim@users.noreply.github.com>
2021-02-09 23:00:31 -0500

    completing the practice on index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
+
+// const newNumbers = numbers.map((x) => x * 2);
+// console.log(newNumbers);
 
 ////Map -Create a new array by doing something with each item in an array.
 // const newNumbers = numbers.map(function (x) {
 //   return x * 2;
 // });
 
+// const newNumbers = numbers.filter( x => x < 10)
+// console.log(newNumbers)
+
 //////Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(function(num) {
 //   return num < 10;
 // });
+
+// const newNumbers = numbers.reduce( (x, y) => x + y);
+// console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
