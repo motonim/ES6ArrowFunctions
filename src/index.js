f1e6224bc7f3cb5288a5cb10601d91def30ab370
Jaeri Park <59538964+motonim@users.noreply.github.com>
2021-02-09 23:10:15 -0500

    completing the emojipedia arrow function challenge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
 // const newNumbers = numbers.map((x) => x * 2);
 // console.log(newNumbers);
